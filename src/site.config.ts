9e96de236895f49fd9ef43eddad8f6ff3e15f12e
lushisang <lushisang@gmail.com>
2025-05-06 10:35:18 +0800

    update

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,7 +3,7 @@ import type { CardListData, Config, IntegrationUserConfig, ThemeUserConfig } fro
 export const theme: ThemeUserConfig = {
   // === Basic configuration ===
   /** Title for your website. Will be used in metadata and as browser tab title. */
-  title: 'Lab',
+  title: '猫工社',
   /** Will be used in index page & copyright declaration */
   author: 'Andy Lu',
   /** Description metadata for your website. Can be used in page metadata. */
