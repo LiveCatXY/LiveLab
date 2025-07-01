e19ad25cfb55437b7b41351fbcc827568ae6ef4e
lushisang <lushisang@gmail.com>
2025-07-01 17:59:56 +0800

    update

--- src/site.config.ts
+++ src/site.config.ts
@@ -57,7 +57,7 @@ export const theme: ThemeUserConfig = {
     /** Enable displaying a “Astro & Pure theme powered” link in your site’s footer. */
     credits: true,
     /** Optional details about the social media accounts for this site. */
-    social: { github: 'https://github.com/andyluss/lab' }
+    social: { github: 'https://github.com/LiveCatXY/LiveLab' }
   },
 
   content: {
