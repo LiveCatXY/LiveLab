6c7c2e7cd294e0234f3d2166f2e6ffcf23950777
lushisang <lushisang@gmail.com>
2025-08-25 15:41:33 +0800

    update

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,7 +3,7 @@ import type { CardListData, Config, IntegrationUserConfig, ThemeUserConfig } fro
 export const theme: ThemeUserConfig = {
   // === Basic configuration ===
   /** Title for your website. Will be used in metadata and as browser tab title. */
-  title: '🧑‍💻逍遥猫工社🤖',
+  title: '🧑‍💻猫工社🤖',
   /** Will be used in index page & copyright declaration */
   author: '😼Andy Lu',
   /** Description metadata for your website. Can be used in page metadata. */
@@ -48,9 +48,9 @@ export const theme: ThemeUserConfig = {
   header: {
     menu: [
       { title: '🏷️标签', link: '/tags' },
-      { title: '帖子', link: '/posts' },
-      { title: '干它', link: '/doit' },
-      { title: '关于', link: '/about' }
+      { title: '📑帖子', link: '/posts' },
+      { title: '🔨干它', link: '/doit' },
+      { title: '🐈关于', link: '/about' }
     ]
   },
 
